1ea26452472180990c0c2466d91796f5632fb77a
烟熏三文鱼 <1678803637@qq.com>
2021-08-07 05:59:01 +0800

    优化项目体验

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-    <main>
+    <main :class="{ [theme]: true }">
         <div class="app-container">
             <div>{{ state.info.hitokoto }}</div>
-            <br>
+            <br />
             「 {{ state.info.from }} 」
             <div></div>
         </div>
@@ -10,11 +10,12 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, reactive } from 'vue'
+import { onMounted, onBeforeUnmount, reactive, ref } from 'vue'
 import axios from 'axios'
+import dayjs from 'dayjs'
 
 const state = reactive<Record<string, any>>({
-  info: {}
+    info: {}
 })
 
 const init = () => {
@@ -27,7 +28,29 @@ const init = () => {
             state.info = res || {}
         })
 }
-console.log(1)
+
+const nowYear = dayjs().format('YYYY')
+const nowMonth = dayjs().format('MM')
+const nowDay = dayjs().format('DD')
+const startDate = dayjs(`${nowYear}-${nowMonth}-${nowDay} 05:58:40`)
+const endDate = dayjs(`${nowYear}-${nowMonth}-${nowDay} 18:00:00`)
+
+const theme = ref('')
+
+const diffTime = () => {
+    const nowDate = dayjs()
+    if (nowDate.isAfter(startDate) && startDate.isBefore(endDate)) {
+        theme.value = 'light'
+    } else {
+        theme.value = 'dark'
+    }
+}
+
+let timer = setInterval(diffTime, 1000)
+onBeforeUnmount(() => {
+    clearInterval(timer)
+})
+
 onMounted(init)
 </script>
 
@@ -37,18 +60,44 @@ onMounted(init)
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
     font-size: 16px;
     font-weight: bold;
     letter-spacing: 2px;
 }
 
-main {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+html,
+body {
+    margin: 0;
+    padding: 0;
 }
 
+main {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.35s;
+}
+
+main.dark {
+    background-color: #2c3e50;
+    color: white;
+}
+
+main.light {
+    background-color: white;
+    color: #2c3e50;
+}
+
+.app-switch-theme {
+    position: fixed;
+    top: 35px;
+    right: 35px;
+}
+
+.app-switch-theme img {
+    width: 45px;
+    height: 45px;
+}
 </style>
